e26ccbdd2cce313d9f580e53842f37c7bac7ab9e
Daniel Li <danli@lcs.on.ca>
2025-01-16 14:29:41 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by 李泽宇 on 2025-01-16.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
