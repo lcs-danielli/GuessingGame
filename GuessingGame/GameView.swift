26ea8c8c3ed788ed92df8cc684a2b263c77c16c1
Daniel Li <danli@lcs.on.ca>
2025-01-16 14:32:18 -0500

    Added starter code for Guessing Game interface.

---
+++ GuessingGame/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  GuessingGame
+//
+//  Created by 李泽宇 on 2025-01-16.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text("Hi")
+    }
+}
+
+#Preview {
+    GameView()
+}
